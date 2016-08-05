f3f7d2614ce1bd6d69be732df3ccd3ffa418e3e1
unchama <nintendowii@jcom.home.ne.jp>
2016-08-05 22:22:52 +0900

    ひな形

---
+++ src/com/github/unchama/buildassist/BuildAssist.java
@@ -0,0 +1,14 @@
+package com.github.unchama.buildassist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BuildAssist  extends JavaPlugin {
+	@Override
+	public void onEnable(){
+
+	}
+	@Override
+	public void onDisable(){
+
+	}
+}
